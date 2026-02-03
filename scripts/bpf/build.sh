29e7b1f05bb75c727736c7dcdd69f22426bb317d
Doğukan Meral <dogukan.meral@yahoo.com>
2026-02-03 05:01:43 +0300

    Fix bpf scripts for current file structure

--- scripts/bpf/build.sh
+++ scripts/bpf/build.sh
@@ -3,16 +3,16 @@
 # Usage: ./build.sh bpf_file.c (default is sched_ext.bpf.c)
 
 # Set the default file
-BPF_FILE=${1:-bpf/sched_ext.bpf.c}
+BPF_FILE=${1:-sample-bpf/sched_ext.bpf.c}
 BASE_FILE=$(basename ${BPF_FILE})
 
 
 # Create the vmlinux header with all the eBPF Linux functions
 # if it doesn'r exist
-if [ ! -f bpf/vmlinux.h ]; then
+if [ ! -f sample-bpf/vmlinux.h ]; then
     echo "Creating vmlinux.h"
-    bpftool btf dump file /sys/kernel/btf/vmlinux format c > bpf/vmlinux.h
+    bpftool btf dump file /sys/kernel/btf/vmlinux format c > sample-bpf/vmlinux.h
 fi
 
 # Compile the scheduler
-clang -target bpf -g -O2 -c $BPF_FILE -o obj/${BASE_FILE}.o -I.
+clang -target bpf -g -O2 -c $BPF_FILE -o bytecode/${BASE_FILE}.o -I.
--- scripts/bpf/start.sh
+++ scripts/bpf/start.sh
@@ -4,28 +4,28 @@
 
 # Build the scheduler if the C file is younger than the .c.o file or if the .c.o file doesn't exist
 # use sched_ext.bpf.c as default
-C_FILE=${1:-bpf/sched_ext.bpf.c}
+C_FILE=${1:-sample-bpf/sched_ext.bpf.c}
 BASE_FILE=$(basename ${C_FILE})
 
 # if --help is passed, print the usage
 
 if [ "$1" = "--help" ]; then
-    echo "Usage: ./start.sh bpf/scheduler_file.c"
+    echo "Usage: ./start.sh sample-bpf/scheduler_file.c"
     # print all the available scheduler files in the directory
     echo "Available scheduler files:"
-    ls -1 bpf/*.bpf.c
+    ls -1 sample-bpf/*.bpf.c
     exit 0
 fi
 
 ./scripts/build.sh $1
 
-sudo ./scripts/stop.sh
+sudo ./scripts/bpf/stop.sh
 
 
 
 
 # Register the scheduler
-sudo bpftool struct_ops register obj/${BASE_FILE}.o /sys/fs/bpf/sched_ext || (echo "Error attaching scheduler, consider calling stop.sh before" || exit 1)
+sudo bpftool struct_ops register bytecode/${BASE_FILE}.o /sys/fs/bpf/sched_ext || (echo "Error attaching scheduler, consider calling stop.sh before" || exit 1)
 
 # Print scheduler name, fails if it isn't registered properly
 cat /sys/kernel/sched_ext/root/ops || (echo "No sched-ext scheduler installed" && exit 1)
