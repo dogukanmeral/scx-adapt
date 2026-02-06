1f6cfc7c1d4a516bac11d62425d56cdf85bc90f7
Doğukan Meral <dogukan.meral@yahoo.com>
2026-02-06 03:23:15 +0300

    Profile runner logic and methods for checking criterias

--- scx-adapt/internal/helper/profile.go
+++ scx-adapt/internal/helper/profile.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v3"
@@ -44,12 +45,12 @@ func (c Config) Swap(i, j int) {
 		procs_disk_io
 */
 
-var VALID_VALUE_REGEX = [...]string{
-	"^(cpu|io|mem)_psi_(some|full)_(10|60|300)$",
-	"^load_avg_(1|5|15)$",
-	"^procs_running$",
-	"^procs_blocked$",
-	"^procs_disk_io$",
+var VALID_VALUE_REGEX = map[string]string{
+	"pressures":    "^(cpu|io|mem)_psi_(some|full)_(10|60|300)$",
+	"loadAvgs":     "^load_avg_(1|5|15)$",
+	"procsRunning": "^procs_running$",
+	"procsBlocked": "^procs_blocked$",
+	"procsDiskIo":  "^procs_disk_io$",
 }
 
 type Criteria struct {
@@ -161,7 +162,72 @@ func YamlConvert(data []byte) (Config, error) {
 	return conf, nil
 }
 
-func RunProfile(profilePath string) error { // TODO: add /etc/scx-adapt and cwd stuff to cmd part, helpers just get absolute paths
+func (c Criteria) SatisfiesLessMore(sysValue float64) bool {
+	// Checking pointers to avoid null-pointer referance
+	if c.MoreThan != nil && c.LessThan != nil {
+		return sysValue > *c.MoreThan && sysValue < *c.LessThan
+	} else if c.MoreThan != nil && c.LessThan == nil {
+		return sysValue > *c.MoreThan
+	} else if c.LessThan != nil && c.MoreThan == nil {
+		return sysValue < *c.LessThan
+	} else {
+		return false
+	}
+}
+
+func (c Criteria) Satisfies() (bool, error) {
+	if b, _ := regexp.MatchString(VALID_VALUE_REGEX["pressures"], c.ValueName); b {
+		pType, pOpt, pSec := ParsePressure(c.ValueName)
+		pValue, err := Pressure(pType, pOpt, pSec)
+
+		if err != nil {
+			return false, err
+		}
+
+		return c.SatisfiesLessMore(pValue), nil
+
+	} else if b, _ := regexp.MatchString(VALID_VALUE_REGEX["loadAvgs"], c.ValueName); b {
+		laMinute := ParseLoadAvg(c.ValueName)
+		laValue, err := LoadAvg(laMinute)
+
+		if err != nil {
+			return false, err
+		}
+
+		return c.SatisfiesLessMore(laValue), nil
+
+	} else if b, _ := regexp.MatchString(VALID_VALUE_REGEX["procsRunning"], c.ValueName); b {
+		pRunValue, err := GetVariableAsInt("/proc/stat", "procs_running")
+
+		if err != nil {
+			return false, err
+		}
+
+		return c.SatisfiesLessMore(float64(pRunValue)), nil
+
+	} else if b, _ := regexp.MatchString(VALID_VALUE_REGEX["procsBlocked"], c.ValueName); b {
+		pBlckValue, err := GetVariableAsInt("/proc/stat", "procs_blocked")
+
+		if err != nil {
+			return false, err
+		}
+
+		return c.SatisfiesLessMore(float64(pBlckValue)), nil
+
+	} else if b, _ := regexp.MatchString(VALID_VALUE_REGEX["procsDiskIo"], c.ValueName); b {
+		pIoValue, err := DiskCurIO()
+
+		if err != nil {
+			return false, err
+		}
+
+		return c.SatisfiesLessMore(float64(pIoValue)), nil
+	} else {
+		return false, nil
+	}
+}
+
+func RunProfile(profilePath string) error { // TODO: add /etc/scx-adapt and isAbsolute stuff to cmd part, helpers just get absolute paths
 	profileData, err := os.ReadFile(profilePath)
 	if err != nil {
 		return fmt.Errorf("Error occured while reading file '%s': %s\n", profilePath, err)
@@ -178,5 +244,29 @@ func RunProfile(profilePath string) error { // TODO: add /etc/scx-adapt and cwd 
 
 	sort.Sort(conf) // Sort schedulers by their priority (smaller int has higher priority)
 
-	// TODO: iterate over schedulers here (considering interval), add helpers for value checking
+	var currentSched Scheduler
+
+NEXT_SCHED:
+	for _, s := range conf.Schedulers {
+		for _, c := range s.Criterias {
+			if b, err := c.Satisfies(); !b || err != nil {
+				continue NEXT_SCHED
+			}
+		}
+
+		if s.Path != currentSched.Path {
+			err := StartScx(s.Path)
+			if err != nil {
+				return err
+			}
+
+			currentSched = s
+		}
+
+		goto SCHED_STARTED
+	}
+
+SCHED_STARTED:
+	time.Sleep(time.Millisecond * time.Duration(conf.Interval))
+	goto NEXT_SCHED
 }
